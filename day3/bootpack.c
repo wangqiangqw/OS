0eeff9c6cf806a09436a8514eca5fca7344b910c
Wang Qiang <wangqiangqw@163.com>
2020-06-30 21:53:00 +0800

    add writemem

--- day3/bootpack.c
+++ day3/bootpack.c
@@ -1,8 +1,15 @@
 void io_hlt(void);
-
+void write_mem8(int addr, int data);
 void HariMain(void)
 {
-    fin:
+    int i;
+    for (i = 0xa0000; i<=0xaffff;i++)
+    {
+        wrtie_mem8(i,i%15);
+    }
+    for(;;)
+    {
         io_hlt();
-        goto fin;
+    }
+        
 }
--- day4/asmfunc.asm
+++ day4/asmfunc.asm
@@ -3,12 +3,20 @@
 
 [FORMAT "WCOFF"]
 [BITS 32]
-
+[INSTRSET "i486p"]
 [FILE "asmfunc.asm"]
 
     GLOBAL  _io_hlt
+    GLOBAL  _write_mem8
+
 [SECTION .text]
 
 _io_hlt:  ; void io_io_hlt(void)
         HLT 
         RET 
+
+_write_mem8:    ; void write_mem8(int addr, int data);
+        MOV     ECX, [ESP+4]
+        MOV     AL,[ESP+8]
+        MOV     [ECX], AL
+        RET
