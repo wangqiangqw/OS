de47f0c308eea084de4f551faa5411c386bacd64
Wang Qiang <wangqiangqw@163.com>
2020-07-01 08:17:48 +0800

    correct set pallette bug

--- day4/bootpack.c
+++ day4/bootpack.c
@@ -1,9 +1,19 @@
 void io_hlt(void);
 void write_mem8(int addr, int data);
+void io_cli(void);
+void io_out8(int port, int data);
+int  io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 void HariMain(void)
 {
     int i;
     char *p=(char*)0xa0000;
+
+    init_palette();
     for(i=0x0000;i<=0xffff;i++)
     {
         
@@ -41,4 +51,23 @@ void init_palette(void)
 	return;
 
 	/* static char ���߂́A�f�[�^�ɂ����g���Ȃ�����DB���ߑ��� */
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = io_load_eflags();
+    io_cli();
+    io_out8(0x3c8,start);
+    for(i = start; i<= end; i++)
+    {
+        io_out8(0x3c9,rgb[0]/4);
+        io_out8(0x3c9,rgb[1]/4);
+        io_out8(0x3c9,rgb[2]/4);
+        rgb+=3;
+        
+    }
+
+    io_store_eflags(eflags);
+    return;
 }
