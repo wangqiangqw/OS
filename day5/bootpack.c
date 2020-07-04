078ad7f98e74fe4f9dddec0f623ff2afdce56f8e
wangqi53 <qiang.qw.wang@siemens-healthineers.com>
2020-07-04 10:27:45 +0800

    add gdt and idt

--- day5/bootpack.c
+++ day5/bootpack.c
@@ -17,12 +17,80 @@ void putfont8_asc(char *vram, int xsize,int x,int y, char c, unsigned char *s);
 void init_mouse_cursor8(char *mouse, char bc);
 void putblock8_8(char *vram, int vxsize, int pxsize,
 	int pysize, int px0, int py0, char *buf, int bxsize);
+	
+void init_gdtidt(void);
+
 struct BOOTINFO
 {
 	char cyls, leds, vmode, reserve;
 	short scrnx,scrny;
 	char *vram;
 };
+struct SEGMENT_DESCRIPTOR
+{
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR
+{
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+
+void init_gdtidt(void)
+{
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)0X00270000;
+	struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR *) 0X0026F800;
+	int i;
+
+	for(i=0;i<8192;i++)
+	{
+		set_segmdesc(gdt+i,0,0,0);
+
+	}
+	set_segmdesc(gdt +1, 0xffffffff,0x00000000,0x4092);
+	set_segmdesc(gdt +2, 0x0007ffff,0x00280000,0x409a);
+	load_gdtr(0xffff,0x00270000);
+
+	for(i=0;i<256;i++)
+	{
+		set_gatedesc(idt + i,0,0,0);
+	}
+	load_ldtr(0x7ff,0x0026f800);
+	return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	if (limit > 0xffffff)
+	{
+		ar |=0x8000;
+		limit /=0x1000;
+	}
+	sd->limit_low = limit &0xffff;
+	sd->base_low  = base & 0xffff;
+	sd->base_mid = (base >> 16) &0xff;
+	sd->access_right = ar &0xff;
+	sd->limit_high = ((limit>>16)&0x0f) | ((ar>>8)&0xf0);
+	sd->base_high	= (base>>24)&0Xff;
+	return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+	gd->offset_low = offset&0xffff;
+	gd->selector = selector;
+	gd->dw_count = (ar>>8)&0xff;
+	gd->access_right 	= ar&0xff;
+	gd->offset_high = (offset>>16)&0xff;
+	return;
+}
 
 #define COL8_000000		0
 #define COL8_FF0000		1
