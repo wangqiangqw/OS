3f2d85f0495961f57247f83caed1f11cc76a7c73
Wang Qiang <wangqiangqw@163.com>
2020-07-27 08:54:17 +0800

    remove SHTCTL parameter from some SHEET function

--- day11/bootpack.c
+++ day11/bootpack.c
@@ -42,18 +42,18 @@ void HariMain(void)
 	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
 	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
 	init_mouse_cursor8(buf_mouse, 99);
-	sheet_slide(shtctl, sht_back, 0, 0);
+	sheet_slide(sht_back, 0, 0);
 	mx = (binfo->scrnx - 16) / 2; /* ��ʒ����ɂȂ�悤�ɍ��W�v�Z */
 	my = (binfo->scrny - 28 - 16) / 2;
-	sheet_slide(shtctl, sht_mouse, mx, my);
-	sheet_updown(shtctl, sht_back,  0);
-	sheet_updown(shtctl, sht_mouse, 1);
+	sheet_slide(sht_mouse, mx, my);
+	sheet_updown( sht_back,  0);
+	sheet_updown(sht_mouse, 1);
 	sprintf(s, "(%3d, %3d)", mx, my);
 	putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 	sprintf(s, "memory %dMB   free : %dKB",
 			memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
-	sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
+	sheet_refresh( sht_back, 0, 0, binfo->scrnx, 48);
 	//sheet_refreshfull(shtctl);
 
     for (;;) {
@@ -69,7 +69,7 @@ void HariMain(void)
 				//putfont8_asc(binfo->vram,binfo->scrnx,0/*mx+m*16*/,16/*my+n*16*/,COL8_FFFFFF,s);
 				boxfill8(buf_back,   binfo->scrnx, COL8_008484,0, 16, 15, 31);
 				putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-				sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
+				sheet_refresh(sht_back, 0, 16, 16, 32);
 				//sheet_refreshfull(shtctl);
 			} else if (fifo8_status(&mousefifo) != 0) 			{
 				i = fifo8_get(&mousefifo);
@@ -87,7 +87,7 @@ void HariMain(void)
 					}
 					boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
 					putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
-					sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
+					sheet_refresh( sht_back, 32, 16, 32 + 15 * 8, 32);
 					mx += mdec.x;
 					my += mdec.y;
 					if (mx < 0) {
@@ -105,8 +105,8 @@ void HariMain(void)
 					sprintf(s, "(%3d, %3d)", mx, my);
 					boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
 					putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-					sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
-					sheet_slide(shtctl, sht_mouse, mx, my);
+					sheet_refresh(sht_back, 0, 0, 80, 16);
+					sheet_slide(sht_mouse, mx, my);
 					//sheet_refreshfull(shtctl);
 				}
 			}			
--- day11/sheet.c
+++ day11/sheet.c
@@ -17,6 +17,7 @@ struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize
     ctl->top = -1;
     for (i = 0; i < MAX_SHEETS; i++) {
         ctl->sheets0[i].flags = 0;
+        ctl->sheets0[i].ctl = ctl;
     }
 err:
     return ctl;
@@ -103,9 +104,10 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
     }
     return;
 }
-void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
+void sheet_updown(struct SHEET *sht, int height)
 {
     int h, old = sht->height;
+    struct SHTCTL *ctl = sht->ctl;
     if (height > ctl->top + 1) {
         height = ctl->top + 1;
     }
@@ -155,10 +157,10 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 
 
 
-void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1)
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1)
 {
     if (sht->height >= 0) {
-        sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
+        sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
     }
     return; 
 }
@@ -185,23 +187,23 @@ void sheet_refreshfull(struct SHTCTL *ctl)
     return;
 }
 
-void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0)
+void sheet_slide( struct SHEET *sht, int vx0, int vy0)
 {
     int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
     sht->vx0 = vx0;
     sht->vy0 = vy0;
     if (sht->height >= 0) { /* �������\�����Ȃ�A�V�����������̏��ɉ����ĉ�ʂ�`������ */
-        sheet_refreshsub(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
-        sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
+        sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
+        sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
         //sheet_refreshfull(ctl);
     }
     return;
 }
 
-void sheet_free(struct SHTCTL *ctl, struct SHEET *sht)
+void sheet_free(struct SHEET *sht)
 {
     if (sht->height >= 0) {
-        sheet_updown(ctl, sht, -1); /* �\�����Ȃ�܂���\���ɂ��� */
+        sheet_updown(sht, -1); /* �\�����Ȃ�܂���\���ɂ��� */
     }
     sht->flags = 0; /* ���g�p�}�[�N */
     return;
